ebdce117c418c02bd2c15cb5489095e689bcb6b7
przemyslawjakubas24 <przemyslawjakubas24@gmail.com>
2025-02-23 20:58:43 +0100

    Small change to index.html

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ document.addEventListener('click', e => {
 const options = {
 	root: null,
 	rootMargin: '0px',
-	threshold: 1.0,
+	threshold: 0.5,
 }
 
 const observer = new IntersectionObserver(entries => {
